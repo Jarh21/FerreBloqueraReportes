fd1b300380c5da4208dac777afe3428dad569983
Jose Antonio Rivero Higuera <jarh18@gmail.com>
2026-02-04 11:51:00 -0400

    Update Solicitudes
    se modifico las notificaciones de las solicitudes de los pagos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -34,10 +34,11 @@ export default function App() {
           toast.info("🔔 Nueva Solicitud Recibida", {
               description: `${data.mensaje}. Monto: ${data.monto} ${data.moneda}`,
               duration: 80000000, 
-              action: {
-                  label: "Ver",
-                  onClick: () => window.location.href = "/consultas" 
-              }
+              /* action: {
+                
+                label: "Ver",
+                  onClick: () => window.location.href = "/dashboard/solicitudes/consulta" 
+              } */
           });
       });
 
